18a7044cb4376836a2b975cb5fe8764a82643ec4
Callum Law <callumlaw1709@outlook.com>
2021-01-09 15:06:24 +1300

    Complete Typing and JSDocs for the library

--- lib/addon.d.ts
+++ lib/addon.d.ts
@@ -1,61 +1,153 @@
-import { ServiceType, ServiceState } from "./index";
+import { ServiceType, ServiceStatus, ServiceStartType } from "./index";
 
-interface IServiceInfo {
+export default class WindowsServiceManager {
 	/**
-	 * The display (friendly) name of the service.
+	 * Get of all of the services the local machine.
+	 * 
+	 * @returns The services on the local machine.
 	 */
-	displayName: string;
+	public static GetServices(): Promise<WindowsServiceManager[]>;
 
 	/**
-	 * The name of the service.
+	 * Get all of the services on the specified machine.
+	 * @param machineName - The name of the machine.
+	 * 
+	 * @returns The services on the specified machine.
 	 */
-	serviceName: string;
+	public static GetServices(machineName: string): Promise<WindowsServiceManager[]>;
+
+	/**
+	 * Get all of the device drives on the local machine.
+	 * 
+	 * @returns The an array of WindowsServiceManagers which are associated with the device drivers on the local machine. 
+	 */
+	public static GetDevices(): Promise<WindowsServiceManager[]>;
+
+	/**
+	 * Get all of the device drives on the specified machine.
+	 * 
+	 * @param machineName - The name of the machine.
+	 * 
+	 * @returns The an array of WindowsServiceManagers which are associated with the device drivers on the specified machine. 
+	 */
+	public static GetDevices(machineName: string): Promise<WindowsServiceManager[]>;
+
+	/**
+	 * Create a new WindowsServiceManager that is associated with the specified service on the local machine.
+	 * @param serviceName - The service to manage.
+	 */
+	public constructor(serviceName: string);
+
+	/**
+	 * Create a new WindowsServiceManager that is associated with the specified service on the specified machine.
+	 * @param serviceName - The service to manage.
+	 * @param machineName - The computer on which the service exists.
+	 */
+	public constructor(serviceName: string, machineName: string);
+
+	/**
+	 * A boolean indicating whether the service can be paused and resumed.
+	 */
+	public readonly CanPauseAndContinue: boolean;
+
+	/**
+	 * A boolean indicating whether the service should be notified when the system is shutting down.
+	 */
+	public readonly CanShutdown: boolean;
+
+	/**
+	 * A boolean indicating whether the service can be stopped after it has started.
+	 */
+	public readonly CanStop: boolean;
+
+	/**
+	 * An array of services that depend on the service associated with this WindowsServiceController instance.
+	 */
+	public readonly DependentServices: WindowsServiceManager[];
+
+	/**
+	 * The friendly name of this service, this can be updated on the system by setting this variable.
+	 */
+	public DisplayName: string;
+
+	/**
+	 * The name of the computer on which this service resides.
+	 */
+	public readonly MachineName: string;
+	
+	/**
+	 * The name of the service which this instance of WindowsServiceManager is associated with.
+	 */
+	public readonly ServiceName: string;
+
+	/**
+	 * All of the services that this
+	 */
+	public readonly ServicesDependedOn: WindowsServiceManager[];
+
+	/**
+	 * How the service starts.
+	 */
+	public readonly StartType: ServiceStartType;
 
 	/**
 	 * The current status of the service.
 	 */
-	status: {
-		/**
-		 * The type of service this is.
-		 */
-		serviceType: ServiceType;
-
-		/**
-		 * The current state of the service.
-		 */
-		currentState: ServiceState;
-
-		/**
-		 * The controls that the service can accept.
-		 */
-		acceptsControls: number;
-	}
-}
-
-export default class NodeJSWindowsServiceManager {
-	/**
-	 * Get of all of the services on this machine.
-	 */
-	public static getServices(): Promise<IServiceInfo[]>;
-
-	public constructor(serviceName: string);
+	public readonly Status: ServiceStatus;
 
 	/**
-	 * Change the start type of the service. This determines at which point in the computers lifecycle the service will start. Read more here
-	 *
-	 * @param desiredStartupType - This is the desired start type. This can be any of the following values:
-	 * 
-	 * Auto - Indicates that the service is to be started (or was started) by the operating system, at system start-up. If an automatically started service depends on a manually started service, the manually started service is also started automatically at system startup.
-	 * 
-	 * Boot - Indicates that the service is a device driver started by the system loader. This value is valid only for device drivers.
-	 * 
-	 * Disabled - Indicates that the service is disabled, so that it cannot be started by a user or application.
-	 * 
-	 * Demand - Indicates that the service is started only manually, by a user (using the Service Control Manager) or by an application.
-	 * 
-	 * System - Indicates that the service is a device driver started by the IOInitSystem function. This value is valid only for device drivers.
-	 * 
-	 * @returns This method returns a promise which resolves to whether or not the 
+	 * Disconnect the WindowsServiceManager from the service and free all resources.
 	 */
-	public changeStartupType(desiredStartupType: "Auto" | "Boot" | "Disabled" | "Demand" | "System"): Promise<never>;
+	public Close(): undefined;
+
+	/**
+	 * Continue a service after it has been paused.
+	 */
+	public Continue(): undefined;
+
+	/**
+	 * Release the unmanaged resources of the WindowsServiceManager instance and optionally release the managed resources aswell.
+	 * @param disposeOfManagedResources - Whether to dispose of the managed resources.
+	 */
+	public Dispose(disposeOfManagedResources?: boolean): undefined;
+
+	/**
+	 * Execute a custom command on the service.
+	 * @param command - An application-defined command flag that indicates which custom command to execute. The value must be between 128 and 256, inclusive.
+	 */
+	public ExecuteCommand(command: number): undefined;
+
+	/**
+	 * Suspend a service's operation
+	 */
+	public Pause(): undefined;
+
+	/**
+	 * Start a service passing no arguments.
+	 */
+	public Start(): undefined;
+
+	/**
+	 * Start a service passing the specified arguments.
+	 * @param arguments - The arguments to pass to the service when starting it.
+	 */
+	public Start(arguments: string[]): undefined;
+
+	/**
+	 * Wait for a specified status.
+	 * @param status - The status to wait for.
+	 * 
+	 * @returns A promise that resolves when the status is reached, or throws if an error is reached.
+	 */
+	public WaitForStatus(status: ServiceStatus): Promise<never>;
+
+	/**
+	 * Wait for a specified status, if the service does not reach that status within the specified timeout an error will be thrown.
+	 * 
+	 * @param status - The status to wait for.
+	 * @param timeout - The time to wait before throwing an error.
+	 * 
+	 * @returns A promise that resolves when the service reaches the desired status, or throws when encountering an error or reaching the timeout.
+	 */
+	public WaitForStatus(status: ServiceStatus, timeout: number): Promise<never>;
 }
--- lib/index.ts
+++ lib/index.ts
@@ -37,24 +37,66 @@ export enum ServiceType {
 	INTERACTIVE_PROCESS = 0x100
 }
 
-export enum ServiceState {
-	STOPPED = 0x1,
-	START_PENDING = 0x2,
-	STOP_PENDING = 0x3,
-	RUNNING = 0x4,
-	CONTINUE_PENDING = 0x5,
-	PAUSE_PENDING = 0x6,
-	PAUSED = 0x7,
+export enum ServiceStartType {
+	/**
+	 * Indicates that the service is a device driver started by the system loader. This value is valid only for device drivers.
+	 */
+	BOOT = 0x0,
+
+	/**
+	 * Indicates that the service is a device driver started by the IOInitSystem function. This value is valid only for device drivers.
+	 */
+	SYSTEM = 0x1,
+
+	/**
+	 * Indicates that the service is to be started (or was started) by the operating system, at system start-up. If an automatically started service depends on a manually started service, the manually started service is also started automatically at system startup.
+	 */
+	AUTOMATIC = 0x2,
+
+	/**
+	 * Indicates that the service is started only manually, by a user (using the Service Control Manager) or by an application.
+	 */
+	MANUAL = 0x3,
+
+	/**
+	 * Indicates that the service is disabled, so that it cannot be started by a user or application.
+	 */
+	DISABLED = 0x4
 }
 
-export enum ServiceControls {
-	ACCEPT_STOP = 0x1,
-	PAUSE_CONTINUE = 0x2,
-	ACCEPT_SHUTDOWN = 0x4,
-	PARAMCHANGE = 0x8,
-	NETBINDCHANGE = 0x10,
-	HARDWAREPROFILECHANGE = 0x20,
-	POWEREVENT = 0x40,
-	SESSIONCHANGE = 0x80,
-	PRESHUTDOWN = 0x100,
+export enum ServiceStatus {
+	/**
+	 * The service is not running.
+	 */
+	STOPPED = 0x1,
+
+	/**
+	 * The service is starting.
+	 */
+	START_PENDING = 0x2,
+
+	/**
+	 * The service is stopping.
+	 */
+	STOP_PENDING = 0x3,
+	
+	/**
+	 * The service is running.
+	 */
+	RUNNING = 0x4,
+
+	/**
+	 * The service continue is pending.
+	 */
+	CONTINUE_PENDING = 0x5,
+
+	/**
+	 * The service pause is pending.
+	 */
+	PAUSE_PENDING = 0x6,
+
+	/**
+	 * The service is paused.
+	 */
+	PAUSED = 0x7,
 }
